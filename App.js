98db6f287820740e7b177526eae336773e87164c
riefer02 <andrew.riefenstahl@gmail.com>
2022-01-30 18:05:53 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+  const [message, setMessage] = useState('Main');
+
+  useEffect(() => setTimeout(() => setMessage('Nomsay'), 1000), [message]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.iOS} />
+      <View style={styles.header}>
+        <Text>Header</Text>
+        <TouchableOpacity
+          onPress={() => setMessage('Work Work Work Zug Zug')}
+          style={styles.button}
+        >
+          <Text>Click Me</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.main}>
+        <Text>{message}</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+        <Image source={require('./assets/peon.jpeg')} />
+      </View>
     </View>
   );
 }
@@ -14,7 +35,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    // flexDirection: 'row',
+  },
+  header: {
+    flex: 1,
+    backgroundColor: 'powderblue',
+  },
+  main: {
+    flex: 1,
+    backgroundColor: 'orange',
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: 'oldlace',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
+    marginBottom: 6,
+    minWidth: '48%',
+    textAlign: 'center',
+  },
+  iOS: {
+    height: 20,
+    width: '100%',
   },
 });
